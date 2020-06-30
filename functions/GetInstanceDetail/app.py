0de68b0d106c63cb80b30b4518dd05d5dde82555
Wang <whuaning@a483e791ddd1.ant.amazon.com>
2020-06-30 23:24:47 +0800

    Deleting alarms when instance terminsted

--- functions/GetInstanceDetail/app.py
+++ functions/GetInstanceDetail/app.py
@@ -10,26 +10,13 @@ import string
 
 def lambda_handler(event, context):
     print(f'Input: {event}')
-    instanceId = event['detail']['instance-id']
     client = boto3.client('ec2')
     response = client.describe_instances(
         InstanceIds=[
-            instanceId,
+            event['InstanceId'],
         ]
     )
     print(f'Response: {response}')
-    imageId = response['Reservations'][0]['Instances'][0]['ImageId']
-    instanceType = response['Reservations'][0]['Instances'][0]['InstanceType']
-    response = client.describe_images(
-        ImageIds=[
-            imageId,
-        ]
-    )
-    print(f'Response: {response}')
-    platformDetails = response['Images'][0]['PlatformDetails']
-    return {
-        'InstanceId': instanceId, 
-        'ImageId': imageId, 
-        'PlatformDetails': platformDetails,
-        'InstanceType': instanceType
-    }
+    event['ImageId'] = response['Reservations'][0]['Instances'][0]['ImageId']
+    event['InstanceType'] = response['Reservations'][0]['Instances'][0]['InstanceType']
+    return event
